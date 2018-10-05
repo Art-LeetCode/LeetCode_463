4671f1e720f3411cf9fca76ba3d3c0b9cc829073
art <art.huang@ehsn.com.tw>
2018-10-05 11:26:17 +0800

    init

---
+++ LeetCode_463_TDD/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LeetCode_463_TDD
+{
+    public class Solution
+    {
+        public int IslandPerimeter(int[,] grid)
+        {
+            var total = 0;
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    var result = GetUnitPerimeter(grid, x, y);
+                    total = total + result;
+                }
+            }
+            return total;
+        }
+
+        private int GetUnitPerimeter(int[,] grid, int x, int y)
+        {
+            var currectUnit = grid[x, y];
+            if (currectUnit != 1) return 0;
+
+            const int maxLength = 4;
+            var north = CheckNorth(grid, x, y);
+            var south = CheckSouth(grid, x, y);
+            var west = CheckWest(grid, x, y);
+            var east = CheckEast(grid, x, y);
+            var currectPerimeter = maxLength - north - south - west - east;
+            return currectPerimeter;
+        }
+
+        private int CheckEast(int[,] grid, int x, int y)
+        {
+            var isExistElement = y < grid.GetLength(1) - 1;
+            return !isExistElement ? 0 : grid[x, y + 1];
+        }
+
+        private int CheckWest(int[,] grid, int x, int y)
+        {
+            var isExistElement = y - 1 >= 0;
+            return !isExistElement ? 0 : grid[x, y - 1];
+        }
+
+        private int CheckSouth(int[,] grid, int x, int y)
+        {
+            var isExistElement = x < grid.GetLength(0) - 1;
+            return !isExistElement ? 0 : grid[x + 1, y];
+        }
+
+        private int CheckNorth(int[,] grid, int x, int y)
+        {
+            var isExistElement = x > 0;
+            return !isExistElement ? 0 : grid[x - 1, y];
+        }
+    }
+}
---
+++ LeetCode_463_TDDTests/SolutionTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using LeetCode_463_TDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_463_TDDTests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void IslandTest1()
+        {
+            var grid = new[,]
+            {
+                {0, 1}
+            };
+
+            var expected = 4;
+            var sut = new Solution();
+            var actual = sut.IslandPerimeter(grid);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IslandTest2()
+        {
+            var grid = new[,]
+            {
+                {0, 1, 1, 1, 0}
+            };
+
+            var expected = 8;
+            var sut = new Solution();
+            var actual = sut.IslandPerimeter(grid);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IslandTest3()
+        {
+            var grid = new[,]
+            {
+                {0, 1, 0,0},
+                {1,1,1,0 },
+                {0,1,0,0 },
+                {1,1,0,0 }
+            };
+
+            var expected = 16;
+            var sut = new Solution();
+            var actual = sut.IslandPerimeter(grid);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
